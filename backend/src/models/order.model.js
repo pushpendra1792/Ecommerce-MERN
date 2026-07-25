c4601436944c9e9f9eb8eb4eebfeeec4650b3103
Pushpendra Pal Singh Rathore <luckyrathore1792@gmail.com>
2026-07-25 09:33:09 +0530

    develop order schema

---
+++ backend/src/models/order.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const userModel = require('./user.model');
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: true
+    },
+
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'products',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            price:{
+                type:Number,
+                required:true
+            }
+        }
+    ],
+
+    totalAmount:{
+        type:Number,
+        required:true
+    },
+
+    status:{
+        type:String
+    },
+
+    shippingAddress:{
+        type:String,
+        required:true
+    }
+
+});
+
+const orderModel = mongoose.Model('orders',orderSchema);
+
+module.exports = orderModel;
