76d06ba7450c7b550a21c07113b37df424d349b5
Pushpendra Pal Singh Rathore <luckyrathore1792@gmail.com>
2026-08-15 13:58:23 +0530

    Fix AxiosConfig, add CORS, rewrite UserActions and route fixes in frontend.... Now Finally Frontend Connected to Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth.routes');
 const productRoutes = require('./routes/product.routes');
@@ -10,6 +11,12 @@ require('dotenv').config();
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}));
+
 app.use(cookieParser());
 
 app.use('/auth', authRoutes);
