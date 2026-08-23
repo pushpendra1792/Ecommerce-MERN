471e0f4495f2ba7eb617637341f7db1979e438e6
Pushpendra Pal Singh Rathore <luckyrathore1792@gmail.com>
2026-08-23 21:41:23 +0530

    add delete user route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,19 +3,20 @@ const userModel = require('../models/user.model')
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const router = express.Router();
-const { loginController, registerController, logoutController, updateUserController } = require('../controllers/auth.controller');
+const { loginController, registerController, logoutController, updateUserController, deleteUserController } = require('../controllers/auth.controller');
 const { checkToken } = require('../middlewares/auth.middleware');
 
 router.post('/register', registerController);
 router.post('/login', loginController);
-router.post('/logout',checkToken, logoutController);
-router.patch('/update-user',checkToken, updateUserController);
+router.post('/logout', checkToken, logoutController);
+router.patch('/update-user', checkToken, updateUserController);
+router.delete('/delete-user', checkToken, deleteUserController);
 
-router.get('/user', checkToken, (req,res)=>{
+router.get('/user', checkToken, (req, res) => {
     res.status(201).json({
-        message:"User fetched Successfully",
-        user:req.user
+        message: "User fetched Successfully",
+        user: req.user
     })
-})  
+})
 
 module.exports = router;
